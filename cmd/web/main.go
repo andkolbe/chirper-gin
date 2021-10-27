c6b3eaf650dc67f1a40ff5135155fe3a9ff53648
Andrew <kolbe1129@gmail.com>
2021-10-26 23:32:34 -0500

    connected to mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 var router *gin.Engine
+var db *sql.DB
 
 func main() {
 	router = gin.Default()
@@ -10,6 +18,22 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 	
 	initializeRoutes()
+
+
+	// Get a database handle
+	var err error 
+	db, err = sql.Open("mysql", dns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("connected to DB!")
+
+	defer db.Close()
 
 	router.Run()
 }
