1257ec436913cef6c9262171a8823154dc27c6d5
Andrew <kolbe1129@gmail.com>
2021-11-10 17:08:43 -0600

    added gin

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import (
-	
-)
-
-
+import "github.com/gin-gonic/gin"
 
 func main() {
-		
+	// creates a new instance of Gin with added middleware, logger, recovery middleware that catches an endpoint if it panics 
+	r := gin.Default()
+
+	// declare a route 
+	r.GET("/ping", func(c *gin.Context) {
+		// use the gin.Context to respond to the incoming request
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// run the router
+	r.Run() 
 }
-
-
-
