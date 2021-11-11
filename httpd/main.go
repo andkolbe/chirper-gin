4587ef486a3ca0c5bac6516643f63e7bb3e4ab8e
Andrew <kolbe1129@gmail.com>
2021-11-10 22:16:10 -0600

    created handlers and chirps packages

--- httpd/main.go
+++ httpd/main.go
@@ -1,18 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/andkolbe/chirper-gin/httpd/handler"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// creates a new instance of Gin with added middleware, logger, recovery middleware that catches an endpoint if it panics 
 	r := gin.Default()
 
 	// declare a route 
-	r.GET("/ping", func(c *gin.Context) {
-		// use the gin.Context to respond to the incoming request
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", handler.PingGet())
 
 	// run the router
 	r.Run() 
---
+++ platform/chirps/chirps.go
@@ -0,0 +1,6 @@
+package chirps
+
+type Chirp struct {
+	Content  string `json:"content"`
+	Location string `json:"location"`
+}
